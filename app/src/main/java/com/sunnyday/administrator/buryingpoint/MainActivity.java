c33bbc5e5773b7388bde9df4b0ecceea78f3ad9c
Administrator <1246771571@qq.com>
2020-02-23 20:24:45 +0800

    Initial commit

---
+++ app/src/main/java/com/sunnyday/administrator/buryingpoint/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sunnyday.administrator.buryingpoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
